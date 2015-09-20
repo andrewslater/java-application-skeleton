143dac578a6757639735ce0a6a8632ddf7a177d0
Andrew Slater <andrewslater@gmail.com>
2015-09-20 02:01:41 -0400

    Refactor ActiveTable rendering logic

--- webapp/src/main/webapp/resources/js/components/ActiveTable.jsx
+++ webapp/src/main/webapp/resources/js/components/ActiveTable.jsx
@@ -1,8 +1,7 @@
 var _ = require("underscore");
 var React = require("react");
 var ReactRouter = require("react-router");
-
-var Link = ReactRouter.Link;
+var Spinner = require("./Spinner");
 
 module.exports = React.createClass({
 
@@ -13,68 +12,82 @@ module.exports = React.createClass({
         }
     },
 
-    render: function() {
-        var tableContent;
-        var fillerRows = [];
+    renderError: function() {
+        var retryLink = null;
 
+        if (this.props.retryCallback) {
+            retryLink = <a href="#" onClick={this.props.retryCallback} className="alert-link">{$.i18n.prop('retry')}</a>;
+        }
+
+        return <tr><td colSpan={this.props.columns.length} className="text-center">
+            <div className="alert alert-warning" role="alert">
+                <span className="glyphicon glyphicon-exclamation-sign gi-3x" aria-hidden="true"></span>
+                {this.props.error.message}
+                {retryLink}
+            </div>
+        </td></tr>
+    },
+
+    renderHeaders: function() {
+        return <tr>
+            {this.props.columns.map(function(column) {
+                var sortIcon = null;
+                var currentSort = null;
+
+                if (column.sortProperty) {
+                    if (this.props.sorts.hasOwnProperty(column.sortProperty)) {
+                        currentSort = this.props.sorts[column.sortProperty].toLowerCase();
+                        sortIcon = <span>&nbsp;<i className={'fa fa-sort-' + currentSort}></i></span>;
+                    }
+                }
+                var headingText = <span>{column.name}{sortIcon}</span>;
+                if (column.sortProperty) {
+                    var newDirection = currentSort == 'asc' ? 'DESC' : 'ASC';
+                    headingText = <a href="javascript:void(0)" onClick={this.props.sortChangeCallback.bind(null, column.sortProperty + ':' + newDirection)}>{headingText}</a>
+                }
+                return <th key={'column-heading-' + column.name}>{headingText}</th>
+            }.bind(this))}
+        </tr>;
+    },
+
+    renderTableRows: function() {
         if (this.props.error) {
-            tableContent = <tr><td colSpan="5" className="text-center">
-                <div className="alert alert-warning" role="alert">
-                    <span className="glyphicon glyphicon-exclamation-sign gi-3x" aria-hidden="true"></span>
-                    {this.props.error.message}
-                    <a href="#" onClick={this.loadPage} className="alert-link">{$.i18n.prop('retry')}</a>
-                </div>
-            </td></tr>;
-        } else if (this.props.page) {
-            var rowCount = 0;
-            tableContent = this.props.page.content.map(function(rowData) {
-                rowCount++;
-                return <tr key={this.props.keyFunction(rowData)}>
-                    {this.props.columns.map(function(column) {
-                        var contents = React.createElement(column.component, {rowData: rowData});
-                        return <td key={'column-contents-' + column.name}>{contents}</td>
-                    })}
-                </tr>;
-            }.bind(this));
-
-            _.range(rowCount, this.props.page.size).map(function(i) {
-                fillerRows.push(<tr><td colSpan={this.props.columns.length}>&nbsp;</td></tr>);
-            }.bind(this));
-
+            return this.renderError();
         }
 
-
-        if (this.props.page === undefined) {
-            tableContent = <tr><td colSpan={this.props.columns.length}><Spinner /></td></tr>
+        if (!this.props.page) {
+            return tableContent = <tr><td colSpan={this.props.columns.length}><Spinner /></td></tr>
         }
 
+        var content = [];
+        var rowCount = 0;
+
+        this.props.page.content.map(function(rowData) {
+            rowCount++;
+            content.push(<tr key={this.props.keyFunction(rowData)}>
+                {this.props.columns.map(function(column) {
+                    var contents = React.createElement(column.component, {rowData: rowData});
+                    return <td key={'column-contents-' + column.name}>{contents}</td>
+                })}
+            </tr>);
+        }.bind(this));
+
+        _.range(rowCount, this.props.page.size).map(function(i) {
+            content.push(<tr><td colSpan={this.props.columns.length}>&nbsp;</td></tr>);
+        }.bind(this));
+
+        return content;
+    },
+
+    render: function() {
         return (
-            <div class="container">
+            <div className="container">
             <div className="row">
                 <div className="col-xs-12">
                     <table className="table table-hover">
                         <thead>
-                        <tr>
-                            {this.props.columns.map(function(column) {
-                                var sortIcon = null;
-                                var currentSort = null;
-
-                                if (column.sortProperty) {
-                                    if (this.props.sorts.hasOwnProperty(column.sortProperty)) {
-                                        currentSort = this.props.sorts[column.sortProperty].toLowerCase();
-                                        sortIcon = <span>&nbsp;<i className={'fa fa-sort-' + currentSort}></i></span>;
-                                    }
-                                }
-                                var headingText = <span>{column.name}{sortIcon}</span>;
-                                if (column.sortProperty) {
-                                    var newDirection = currentSort == 'asc' ? 'DESC' : 'ASC';
-                                    headingText = <a href="javascript:void(0)" onClick={this.props.sortChangeCallback.bind(null, column.sortProperty + ':' + newDirection)}>{headingText}</a>
-                                }
-                                return <th key={'column-heading-' + column.name}>{headingText}</th>
-                            }.bind(this))}
-                        </tr>
-                        {tableContent}
-                        {fillerRows}
+                        {this.renderHeaders()}
+                        {this.renderTableRows()}
                         </thead>
                     </table>
                 </div>
